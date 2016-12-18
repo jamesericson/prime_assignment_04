99260bd3e302fa2f120dc4a4901dae1958963b22
James Ericson <jamesericson@gmail.com>
2016-12-18 00:03:02 -0600

    add display to dom and connection to db

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,5 +1,57 @@
 
 $(document).ready(function(){
   console.log('JS & JQ');
-
+  init();
 });
+
+function init(){
+  updateList();
+  $('#add-item-button').on('click', addItem);
+  $(document).keypress(function(e){if(e.which == 13){addItem();} });
+  $(document).on('click', '.completed-button', selectComplete);
+} // end init()
+
+function addItem(){
+  console.log('add Item');
+  $.ajax({
+    type: 'POST',
+    url: '/newItem',
+    data: send = {
+        newItem: $('#add-item').val()
+      },
+    success: (function(response){
+      console.log('back from server', response);
+      updateList();
+    }),
+    error: (function(err){
+      console.log(err);
+    })
+  });//end ajax
+  $('#add-item').val('');
+
+} //end addItem()
+
+function updateList(){
+  $.ajax({
+    type: 'GET',
+    url: '/getList',
+    success: (function(response){
+      console.log('back from server', response);
+      var outputText = '<table>'
+      // <tr><td></td><td>check complete</td><td>delete</td></tr>
+      for (var i = 0; i < response.length; i++) {
+        outputText += '<tr><td>'+ response[i].item +'</td><td class="completed-button" >complete?</td><td>delete</td></tr>';
+      }
+      outputText += '</table>';
+      $('#todoList').html(outputText);
+    }),
+    error: (function(err){
+      console.log(err);
+    })
+  });//end ajax
+} //end updateList()
+
+function selectComplete(){
+  console.log('in selectComplete');
+  $(this).parent().addClass('completed');
+} // end selectComplete()
--- server/app.js
+++ server/app.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var pg = require('pg');
+
+
 var app = express();
-var port = 3001;
+var port = process.env.PORT || 3001;
+var connectionString = 'postgres://localhost:5432/Todo';
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
@@ -14,3 +18,43 @@ app.listen(port, function(){
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, '../views/index.html'));
 });
+
+app.post('/newItem', function(req, res){
+  console.log('hit newItem route');
+  //cont to DB
+  pg.connect( connectionString, function(err, client, done){
+    if( err ){
+      console.log(err);
+    } else {
+      console.log('connected to DB');
+      // use wildcards to insert record
+      client.query( 'INSERT INTO todolist (item) VALUES ($1)', [req.body.newItem] );
+      done();
+      res.send('ok');
+    }//end if else
+  });// end connect
+});//end post newItem
+
+app.get('/getList', function(req, res){
+  console.log('hit getList route');
+  // connect to DB
+  pg.connect( connectionString, function( err, client, done){
+    if(err){
+      console.log(err);
+    } else {
+      console.log('connected to DB');
+      var query = client.query( 'SELECT item FROM todolist' );
+      //array for list
+      var allList = [];
+      query.on( 'row', function( row ){
+        allList.push (row);
+      });
+      query.on( 'end', function(){
+      done();
+      console.log( allList);
+
+      res.send( allList );
+      });
+    } // end if else
+  }); // end connect
+}); //end get getlist
